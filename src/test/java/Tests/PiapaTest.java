6e8fbff0d603ecfaf94a3947025abff577311bf9
niakoo <nikitarachkov98@gmail.com>
2023-02-15 15:13:45 +0300

    ada

--- src/test/java/Tests/PiapaTest.java
+++ src/test/java/Tests/PiapaTest.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-
 import static org.testng.Assert.*;
 
 public class PiapaTest extends BaseTest {
