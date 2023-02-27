29b78fcc218a6c2540222581e7c9e349b8fb1c4c
niakoo <nikitarachkov98@gmail.com>
2023-02-27 18:13:53 +0300

    добавил тест auto-complete-test

--- src/test/java/Pages/WidgetsPage.java
+++ src/test/java/Pages/WidgetsPage.java
@@ -14,12 +14,27 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElemen
 public class WidgetsPage {
     @FindBy(xpath = "//li[contains(., 'Accordian')]")
     public WebElement accordian;
+    @FindBy(xpath = "//li[contains(., 'Auto Complete')]")
+    public WebElement autoComplete;
+
     @FindBy(xpath = "//div[@id = 'section1Heading']")
     private WebElement section1Heading;
     @FindBy(xpath = "//div[@id = 'section2Heading']")
     private WebElement section2Heading;
     @FindBy(xpath = "//div[@id = 'section3Heading']")
     private WebElement section3Heading;
+
+    @FindBy(xpath = "//input[@id = 'autoCompleteMultipleInput']")
+    private WebElement autoCompleteMultipleInput;
+    @FindBy(xpath = "//input[@id = 'autoCompleteSingleInput']")
+    private WebElement autoCompleteSingleInput;
+    @FindBy(xpath = "//div[contains(@class, 'clear-indicator')]")
+    private WebElement clearIndicator;
+    @FindBy(xpath = "//div[contains(@class, 'container--is-multi')]")
+    public WebElement containerIsMulti;
+    @FindBy(xpath = "//div[contains(@class, 'auto-complete__value-container auto-complete__value-container--has-value')]")
+    public WebElement containerIsSingle;
+
 
     public WebElement getSectionCollapseShow(String name) {
         return new WebDriverWait(driver, Duration.ofSeconds(10))
@@ -45,6 +60,38 @@ public class WidgetsPage {
 
     public void clickSection3Heading() {
         section3Heading.click();
+    }
+
+    private WebElement getElementDropDownMenu(String name) {
+        return new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(presenceOfElementLocated(
+                        By.xpath("//div[contains(@id, 'react-select') and .='" + name + "']")
+                ));
+    }
+
+    public void sendKeysAndClickMultipleInput(String send, String completeOption) {
+        autoCompleteMultipleInput.sendKeys(send);
+        getElementDropDownMenu(completeOption).click();
+    }
+
+    public void sendKeysAndClickSingleInput(String send, String completeOption) {
+        autoCompleteSingleInput.sendKeys(send);
+        getElementDropDownMenu(completeOption).click();
+    }
+
+    private WebElement getRemoveElement(String name) {
+        return new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(presenceOfElementLocated(
+                        By.xpath("//div[contains(@class, 'label') and (text() = '" + name + "')]//following-sibling::div[contains(@class, 'remove')]")
+                ));
+    }
+
+    public void clickRemoveElement(String subjectsRemove) {
+        getRemoveElement(subjectsRemove).click();
+    }
+
+    public void clickClearIndicator() {
+        clearIndicator.click();
     }
 
     private WebDriver driver;
--- src/test/java/Tests/WidgetsTest.java
+++ src/test/java/Tests/WidgetsTest.java
@@ -2,11 +2,12 @@ package Tests;
 
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
 public class WidgetsTest extends BaseTest {
     @Test
-    public void accordianTest() throws InterruptedException {
+    public void accordianTest() {
 
         driver.get("https://demoqa.com/");
 
@@ -31,5 +32,43 @@ public class WidgetsTest extends BaseTest {
 
         widgetsPage.clickSection3Heading();
         assertNotNull(widgetsPage.getSectionCollapse("Why do we use it?"));
+    }
+
+    @Test
+    public void autoCompleteTest() {
+
+        driver.get("https://demoqa.com/");
+
+        landingPage.clickCategoryCardsWidgets();
+
+        clickJS(widgetsPage.autoComplete);
+
+        widgetsPage.sendKeysAndClickMultipleInput("e", "Red");
+        widgetsPage.sendKeysAndClickMultipleInput("q", "Aqua");
+        widgetsPage.sendKeysAndClickMultipleInput("w", "White");
+        assertEquals(widgetsPage.containerIsMulti.getText(),
+                "Red\n" +
+                        "Aqua\n" +
+                        "White");
+
+        widgetsPage.clickRemoveElement("White");
+        widgetsPage.clickRemoveElement("Red");
+        assertEquals(widgetsPage.containerIsMulti.getText(), "Aqua");
+
+
+        widgetsPage.clickClearIndicator();
+        assertEquals(widgetsPage.containerIsMulti.getText(), "");
+
+        widgetsPage.sendKeysAndClickMultipleInput("t", "Magenta");
+        widgetsPage.sendKeysAndClickMultipleInput("o", "Indigo");
+        assertEquals(widgetsPage.containerIsMulti.getText(),
+                "Magenta\n" +
+                        "Indigo");
+
+        widgetsPage.sendKeysAndClickSingleInput("b", "Black");
+        assertEquals(widgetsPage.containerIsSingle.getText(), "Black");
+
+        widgetsPage.sendKeysAndClickSingleInput("h", "White");
+        assertEquals(widgetsPage.containerIsSingle.getText(), "White");
     }
 }
